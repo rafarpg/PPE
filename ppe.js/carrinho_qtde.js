c65b3204bf5e9bd004b0a0473791f63324c26ccd
rafarpg <rafaelpassosgol@gmail.com>
2023-06-01 10:53:49 -0300

    qtde carrinho

---
+++ ppe.js/carrinho_qtde.js
@@ -0,0 +1,126 @@
+function dimQuantidade() {
+    var quantityElement = document.getElementById("quantidade");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+
+  function aumQuantidade() {
+    var quantityElement = document.getElementById("quantidade");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function dimQuantidade2() {
+  var quantityElement = document.getElementById("quantidade2");
+  var currentQuantity = parseInt(quantityElement.innerText);
+  if (currentQuantity > 0) {
+    quantityElement.innerText = currentQuantity - 1;
+  }
+}
+
+function aumQuantidade2() {
+  var quantityElement = document.getElementById("quantidade2");
+  var currentQuantity = parseInt(quantityElement.innerText);
+  quantityElement.innerText = currentQuantity + 1;
+}
+function dimQuantidade3() {
+    var quantityElement = document.getElementById("quantidade3");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+  
+  function aumQuantidade3() {
+    var quantityElement = document.getElementById("quantidade3");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function dimQuantidade4() {
+    var quantityElement = document.getElementById("quantidade4");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+  
+  function aumQuantidade4() {
+    var quantityElement = document.getElementById("quantidade4");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function dimQuantidade5() {
+    var quantityElement = document.getElementById("quantidade5");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+  
+  function aumQuantidade5() {
+    var quantityElement = document.getElementById("quantidade5");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function dimQuantidade6() {
+    var quantityElement = document.getElementById("quantidade6");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+  
+  function aumQuantidade6() {
+    var quantityElement = document.getElementById("quantidade6");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function dimQuantidade7() {
+    var quantityElement = document.getElementById("quantidade7");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    if (currentQuantity > 0) {
+      quantityElement.innerText = currentQuantity - 1;
+    }
+  }
+  
+  function aumQuantidade7() {
+    var quantityElement = document.getElementById("quantidade7");
+    var currentQuantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = currentQuantity + 1;
+  }
+  function Remove()
+  {
+    var quantidadeElement = document.getElementById("quantidade");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove2()
+  {
+    var quantidadeElement = document.getElementById("quantidade2");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove3()
+  {
+    var quantidadeElement = document.getElementById("quantidade3");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove4()
+  {
+    var quantidadeElement = document.getElementById("quantidade4");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove5()
+  {
+    var quantidadeElement = document.getElementById("quantidade5");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove6()
+  {
+    var quantidadeElement = document.getElementById("quantidade6");
+    quantidadeElement.textContent = "0";
+  }
+  function Remove7()
+  {
+    var quantidadeElement = document.getElementById("quantidade7");
+    quantidadeElement.textContent = "0";
+  }
